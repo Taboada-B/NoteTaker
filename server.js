2af44afcea5e97effba051d147999079c8060066
Bryan Taboada <b.taboada123@gmail.com>
2024-07-21 10:36:42 -0600

    restructure files

---
+++ server.js
@@ -0,0 +1,28 @@
+// Dependencies
+const express = require ('express')
+const path = require('path');
+const fs = require('fs');
+
+// Initializing express.js
+const PORT = 3001;
+const app = express();
+
+// middleware
+app.use(express.static(__dirname));
+
+
+
+
+
+app.listen(PORT, () =>
+    console.log(`Example app listening at http://localhost:${PORT}`)
+  );
+
+
+// Setup data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+//Require routes file
+require('./routes/routes')(app);
